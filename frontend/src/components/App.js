1744f9309da08db786bf931be70d70b3a6451723
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2019-09-12 22:40:43 -0600

    Set up login and authentication and the HOC.
    Added dependencies, edited the readme file.

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -3,108 +3,23 @@ import "./App.css";
 import { connect } from "react-redux";
 import { initial } from "../actions";
 // import { GlobalStyle, AppContainer, H1 } from "../styles";
-import Village from "./Village";
+import MainPage from "./components/chores";
+import withAuthenticate from "../src/components/authentication/withAuthenticate";
+import Login from "../src/components/login/Login";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      login: {
-        email: "",
-        password: ""
-      } /*,
-      mode: "Add",
-      id: ""*/
-    };
+  constructor() {
+    super();
+    this.state = {};
   }
-
-  componentDidMount() {
-    this.props.initial();
-  }
-
-  //   addSmurf = event => {
-  //     event.preventDefault();
-  //     this.props.addSmurfs(this.state.smurf);
-  //     this.defaultForm();
-  //   };
-
-  //   updateSmurf = () => {
-  //     this.props.updateSmurfs(this.state.smurf, this.state.id);
-  //     this.defaultForm();
-  //   };
-
-  //   deleteSmurf = id => {
-  //     this.props.deleteSmurfs(id);
-  //   };
-
-  //   toggleMode = (id, name) => {
-  //     this.setState({
-  //       ...this.state,
-  //       mode: "Update",
-  //       id: id,
-  //       smurf: {
-  //         ...this.state.smurf,
-  //         name: name
-  //       }
-  //     });
-  //   };
-
-  //   defaultForm = () => {
-  //     this.setState({
-  //       ...this.state,
-  //       mode: "Add",
-  //       smurf: {
-  //         ...this.state.smurf,
-  //         name: "",
-  //         age: "",
-  //         height: ""
-  //       }
-  //     });
-  //   };
-
-  handleChange = event => {
-    event.preventDefault();
-    this.setState({
-      login: {
-        ...this.state.login,
-        [event.target.name]: event.target.value
-      }
-    });
-  };
-
   render() {
     return (
-      <React.Fragment>
-        <GlobalStyle />
-        <AppContainer>
-          <H1>Welcome to Home Chore Tracker</H1>
-          <Village
-            mode={this.state.mode}
-            toggleMode={this.toggleMode}
-            handleChange={this.handleChange}
-            name={this.state.smurf.name}
-            age={this.state.smurf.age}
-            height={this.state.smurf.height}
-            addSmurf={this.addSmurf}
-            updateSmurf={this.updateSmurf}
-            deleteSmurf={this.deleteSmurf}
-            smurfs={this.props.smurfs}
-            defaultForm={this.defaultForm}
-          />
-        </AppContainer>
-      </React.Fragment>
+      <div className="App">
+        <ComponentFromWithAuthenticate />;
+      </div>
     );
   }
 }
 
-const mapStateToProps = state => ({
-  smurfs: state.smurfs,
-  fetchingSmurfs: state.fetchingSmurfs
-});
-
-export default connect(
-  mapStateToProps,
-  {
-    initial
-  }
-)(App);
+const ComponentFromWithAuthenticate = withAuthenticate(MainPage)(Login);
+export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-import thunk from 'redux-thunk';
-import logger from 'redux-logger';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import { reducer } from './reducers';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import { reducer } from "./reducers";
+
+import "font-awesome/css/font-awesome.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import $ from "jquery";
+import Popper from "popper.js";
 
 const store = createStore(reducer, applyMiddleware(thunk, logger));
 
@@ -14,5 +20,5 @@ ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
